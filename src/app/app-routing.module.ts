e7a23dce8590593cd041996c403949946ef2210e
Trung Ho Ngoc - macpro 13 2018 <trung0194@gmai.com>
2021-01-01 19:01:42 +0700

    Add ant design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,6 @@ import { Routes, RouterModule } from '@angular/router'
 const routes: Routes = []
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
